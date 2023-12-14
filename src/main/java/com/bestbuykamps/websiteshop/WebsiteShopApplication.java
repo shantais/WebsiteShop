3532be93fba36270080e016f8a3abf86a618103d
Anna Waczyńska <waczynska.a@gmail.com>
2023-12-14 12:07:43 +0100

    PK-1 Init

---
+++ src/main/java/com/bestbuykamps/websiteshop/WebsiteShopApplication.java
@@ -0,0 +1,13 @@
+package com.bestbuykamps.websiteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestbuykamps/websiteshop/WebsiteShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestbuykamps.websiteshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

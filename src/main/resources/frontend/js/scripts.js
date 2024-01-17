66ce42f5569053e1c6fc16d317055b17c6d9b19d
shantais <waczynska.a@gmail.com>
2024-01-17 19:56:44 +0100

    frontend

---
+++ src/main/resources/frontend/js/scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("login").addEventListener("click", function() {
+    window.location.href = "Pages/LOGIN_PAGE.html";});
+
+console.log('Oddzielny plik JavaScript')
+
+document.getElementById("cart").addEventListener("click", function() { window.location.href = "CART_PAGE.html";});

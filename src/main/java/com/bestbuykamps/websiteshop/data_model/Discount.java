700d7d11f9736fa4a14e06222f45a1219541ae05
Michal <19Sandriel93@gmail.com>
2024-01-22 19:37:38 +0100

    Dodanie entity dla enuma

--- src/main/java/com/bestbuykamps/websiteshop/data_model/Discount.java
+++ src/main/java/com/bestbuykamps/websiteshop/data_model/Discount.java
@@ -3,18 +3,42 @@ package com.bestbuykamps.websiteshop.data_model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="Discount")
+@Table(name = "discount")
 public class Discount {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type_of_discount", nullable = false)
+    private DiscountType typeOfDiscount;
+
+    @Column(name = "discount", nullable = false)
+    private Double discount;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DiscountType getTypeOfDiscount() {
+        return typeOfDiscount;
+    }
+
+    public void setTypeOfDiscount(DiscountType typeOfDiscount) {
+        this.typeOfDiscount = typeOfDiscount;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
 }
-
-
-/*
-CREATE TABLE discount (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    typeOfDiscount ENUM('percent', 'value') NOT NULL,
-    discount DOUBLE NOT NULL
-);
- */

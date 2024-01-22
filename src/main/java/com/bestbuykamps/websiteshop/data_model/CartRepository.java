f0c6a3a37e1550feff7b987c7864a61b5eb0b917
shantais <waczynska.a@gmail.com>
2024-01-22 20:54:10 +0100

    controller service

--- src/main/java/com/bestbuykamps/websiteshop/data_model/CartRepository.java
+++ src/main/java/com/bestbuykamps/websiteshop/data_model/CartRepository.java
@@ -1,6 +1,8 @@
 package com.bestbuykamps.websiteshop.data_model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
+//@Repository
+//public interface CartRepository extends JpaRepository<Cart, Long> {
+//}
--- src/main/java/com/bestbuykamps/websiteshop/web_controller/Product_Controller.java
+++ src/main/java/com/bestbuykamps/websiteshop/web_controller/Product_Controller.java
@@ -1,23 +1,30 @@
 package com.bestbuykamps.websiteshop.web_controller;
 
+import com.bestbuykamps.websiteshop.business_service.ProductService;
 import com.bestbuykamps.websiteshop.data_model.Product;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
-@RequestMapping("/product")
+@RestController
+//@RequestMapping("/products")
 public class Product_Controller {
+
+    private final ProductService productService;
 
     // tutaj wypełnić listę danymi z bazy, tymczasowo wpisałam na sztywno
     private static final List<Product> PRODUCTS = Arrays.asList(
             new Product(1L, "The Sims"),
             new Product(2L, "Hogwarts Legacy")
     );
+
+    public Product_Controller(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("productId")
     public Product getProducts (@PathVariable("productId") Long productId){
@@ -27,6 +34,11 @@ public class Product_Controller {
                 .orElseThrow(()->new IllegalStateException(
                         "Game " + productId + " does not exist!"
                 ));
+    }
+
+    @GetMapping(path="products")
+    public List<Product> productList() {
+        return PRODUCTS;
     }
 
 }

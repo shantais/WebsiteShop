0ced670ec60757830e7e2ad01889a716172db349
Michal <19Sandriel93@gmail.com>
2024-01-23 18:10:25 +0100

    Połączenie bazy z frontem - wersja 1.1

--- src/main/java/com/bestbuykamps/websiteshop/data_model/Discount.java
+++ src/main/java/com/bestbuykamps/websiteshop/data_model/Discount.java
@@ -3,14 +3,46 @@ package com.bestbuykamps.websiteshop.data_model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="Discount")
+@Table(name = "discount")
 public class Discount {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type_of_discount", nullable = false)
+    private DiscountType typeOfDiscount;
+
+    @Column(name = "discount", nullable = false)
+    private Double discount;
+
+    // Konstruktory, gettery, settery i inne metody
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DiscountType getTypeOfDiscount() {
+        return typeOfDiscount;
+    }
+
+    public void setTypeOfDiscount(DiscountType typeOfDiscount) {
+        this.typeOfDiscount = typeOfDiscount;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
 }
-
-
 /*
 CREATE TABLE discount (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,

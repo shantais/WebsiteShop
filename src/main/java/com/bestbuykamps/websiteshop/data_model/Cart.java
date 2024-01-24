6b076c91f71147f7d5c456787ec7ec8c74e63fd3
sebas <waliszeksebastian@gmail.com>
2024-01-24 19:36:12 +0100

    update Cart.java

--- src/main/java/com/bestbuykamps/websiteshop/data_model/Cart.java
+++ src/main/java/com/bestbuykamps/websiteshop/data_model/Cart.java
@@ -1,4 +1,39 @@
 package com.bestbuykamps.websiteshop.data_model;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "cart")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "id=" + id +
+                ", user=" + userId +
+                '}';
+    }
 }

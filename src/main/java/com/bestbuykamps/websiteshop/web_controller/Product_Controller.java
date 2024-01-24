bfe2a9ecc230457dcab63a2389414a57953e7140
shantais <waczynska.a@gmail.com>
2024-01-24 20:52:22 +0100

    thymelief support w pomie!!!!

--- src/main/java/com/bestbuykamps/websiteshop/web_controller/Product_Controller.java
+++ src/main/java/com/bestbuykamps/websiteshop/web_controller/Product_Controller.java
@@ -3,13 +3,14 @@ package com.bestbuykamps.websiteshop.web_controller;
 import com.bestbuykamps.websiteshop.business_service.ProductService;
 import com.bestbuykamps.websiteshop.data_model.Product;
 import com.bestbuykamps.websiteshop.data_model.ProductRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/products")
 public class Product_Controller {
 

c9a5d92fc5e9c19e70fc729ea572112093cadced
shantais <waczynska.a@gmail.com>
2024-01-25 18:46:05 +0100

    base for warehouse service

--- src/main/java/com/bestbuykamps/websiteshop/business_service/CartService.java
+++ src/main/java/com/bestbuykamps/websiteshop/business_service/CartService.java
@@ -1,12 +1,14 @@
-//package com.bestbuykamps.websiteshop.business_service;
-//
-//import com.bestbuykamps.websiteshop.data_model.CartRepository;
-//import com.bestbuykamps.websiteshop.data_model.ProductRepository;
-//
-//public class CartService {
-//    private final CartRepository cartRepository;
-//
-//    public CartService(CartRepository cartRepository) {
-//        this.cartRepository = cartRepository;
-//    }
-//}
+package com.bestbuykamps.websiteshop.business_service;
+
+import com.bestbuykamps.websiteshop.data_model.CartRepository;
+import com.bestbuykamps.websiteshop.data_model.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+    private final CartRepository cartRepository;
+
+    public CartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+}
---
+++ src/main/java/com/bestbuykamps/websiteshop/business_service/WarehouseService.java
@@ -0,0 +1,7 @@
+package com.bestbuykamps.websiteshop.business_service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WarehouseService {
+}

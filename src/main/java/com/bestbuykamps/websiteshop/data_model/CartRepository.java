ca2da63c2578e4bddeadc0fdb6078ed2065987c6
WallyLDZ <waliszeksebastian@gmail.com>
2024-01-25 18:41:12 +0100

    update CartRepository.java and CartService.java

--- src/main/java/com/bestbuykamps/websiteshop/data_model/CartRepository.java
+++ src/main/java/com/bestbuykamps/websiteshop/data_model/CartRepository.java
@@ -3,6 +3,6 @@ package com.bestbuykamps.websiteshop.data_model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//public interface CartRepository extends JpaRepository<Cart, Long> {
-//}
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}

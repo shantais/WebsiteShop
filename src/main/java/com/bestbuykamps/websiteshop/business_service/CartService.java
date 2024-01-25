e913b73739674170918ef923d10ebf3f0bee37d6
WallyLDZ <waliszeksebastian@gmail.com>
2024-01-25 18:42:48 +0100

    update  CartService.java

--- src/main/java/com/bestbuykamps/websiteshop/business_service/CartService.java
+++ src/main/java/com/bestbuykamps/websiteshop/business_service/CartService.java
@@ -1,7 +1,7 @@
 package com.bestbuykamps.websiteshop.business_service;
 
 import com.bestbuykamps.websiteshop.data_model.CartRepository;
-import com.bestbuykamps.websiteshop.data_model.ProductRepository;
+
 
 public class CartService {
     private final CartRepository cartRepository;

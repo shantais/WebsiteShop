9b4871e666247cea3ff458633a5a454d2feb045c
shantais <waczynska.a@gmail.com>
2024-01-25 20:26:54 +0100

    base for cart service and controller

--- src/main/java/com/bestbuykamps/websiteshop/business_service/ProductService.java
+++ src/main/java/com/bestbuykamps/websiteshop/business_service/ProductService.java
@@ -1,6 +1,5 @@
 package com.bestbuykamps.websiteshop.business_service;
 
-import com.bestbuykamps.websiteshop.data_model.Discount;
 import com.bestbuykamps.websiteshop.data_model.Product;
 import com.bestbuykamps.websiteshop.data_model.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;

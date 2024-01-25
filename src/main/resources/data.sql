7f1dcce6fcf9b640c537d033fd3e012430cac978
Michal <19Sandriel93@gmail.com>
2024-01-25 18:55:29 +0100

    dodanie imagePath do produktu , poprawa insterow pod nowy format danych

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,35 +6,35 @@ INSERT INTO discount (type_of_discount, discount_value) VALUES
     ('VALUE', 50.0);
 
 -- Inserty dla produktów
-INSERT INTO product (name, category, price, discount_id) VALUES
-    ('The Witcher 3', 'Gry Komputerowe', 59.99, 1);
+INSERT INTO product (name, category, price, discount_id, image_path) VALUES
+    ('The Witcher 3', 'Gry Komputerowe', 59.99, 1 , '/images/callofduty.jpg');
 
-INSERT INTO product (name, category, price, discount_id) VALUES
-    ('Cyberpunk 2077', 'Gry Komputerowe', 49.99, 2);
+INSERT INTO product (name, category, price, discount_id, image_path) VALUES
+    ('Cyberpunk 2077', 'Gry Komputerowe', 49.99, 2, '/images/callofduty.jpg');
 
-INSERT INTO product (name, category, price, discount_id) VALUES
-    ('Assassin''s Creed Valhalla', 'Gry Komputerowe', 69.99, 1);
+INSERT INTO product (name, category, price, discount_id,image_path) VALUES
+    ('Assassin''s Creed Valhalla', 'Gry Komputerowe', 69.99, 1, '/images/callofduty.jpg');
 
-INSERT INTO product (name, category, price, discount_id) VALUES
-    ('DOOM Eternal', 'Gry Komputerowe', 39.99, 2);
+INSERT INTO product (name, category, price, discount_id,image_path) VALUES
+    ('DOOM Eternal', 'Gry Komputerowe', 39.99, 2, '/images/callofduty.jpg');
 
-INSERT INTO product (name, category, price, discount_id) VALUES
-    ('FIFA 22', 'Gry Komputerowe', 59.99, 1);
+INSERT INTO product (name, category, price, discount_id,image_path) VALUES
+    ('FIFA 22', 'Gry Komputerowe', 59.99, 1, '/images/callofduty.jpg');
 
-INSERT INTO product (name, category, price, discount_id) VALUES
-    ('Minecraft', 'Gry Komputerowe', 29.99, 2);
+INSERT INTO product (name, category, price, discount_id,image_path) VALUES
+    ('Minecraft', 'Gry Komputerowe', 29.99, 2, '/images/callofduty.jpg');
 
-INSERT INTO product (name, category, price, discount_id) VALUES
-    ('World of Warcraft', 'Gry Komputerowe', 49.99, 1);
+INSERT INTO product (name, category, price, discount_id,image_path) VALUES
+    ('World of Warcraft', 'Gry Komputerowe', 49.99, 1, '/images/callofduty.jpg');
 
-INSERT INTO product (name, category, price, discount_id) VALUES
-    ('Overwatch', 'Gry Komputerowe', 39.99, 2);
+INSERT INTO product (name, category, price, discount_id,image_path) VALUES
+    ('Overwatch', 'Gry Komputerowe', 39.99, 2, '/images/callofduty.jpg');
 
-INSERT INTO product (name, category, price, discount_id) VALUES
-    ('The Elder Scrolls V: Skyrim', 'Gry Komputerowe', 39.99, 1);
+INSERT INTO product (name, category, price, discount_id,image_path) VALUES
+    ('The Elder Scrolls V: Skyrim', 'Gry Komputerowe', 39.99, 1, '/images/callofduty.jpg');
 
-INSERT INTO product (name, category, price, discount_id) VALUES
-    ('Stardew Valley', 'Gry Komputerowe', 14.99, 2);
+INSERT INTO product (name, category, price, discount_id,image_path) VALUES
+    ('Stardew Valley', 'Gry Komputerowe', 14.99, 2, '/images/callofduty.jpg');
 
 
 -- inserty dla warehouse

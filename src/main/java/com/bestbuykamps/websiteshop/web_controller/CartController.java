11ecfd141da3aedf3e93eb88aa788ccfd2a99485
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-02-05 20:11:45 +0100

    adding changes to CartController

--- src/main/java/com/bestbuykamps/websiteshop/web_controller/CartController.java
+++ src/main/java/com/bestbuykamps/websiteshop/web_controller/CartController.java
@@ -2,30 +2,29 @@ package com.bestbuykamps.websiteshop.web_controller;
 
 import com.bestbuykamps.websiteshop.business_service.CartService;
 import com.bestbuykamps.websiteshop.data_model.Product;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cart")
 public class CartController {
     private final CartService cartService;
+    private Long productId;
 
     public CartController(CartService cartService) {
         this.cartService = cartService;
     }
 
     // dodanie do koszyka
-    @RequestMapping(path = "/products", method = RequestMethod.POST)
-    public void addProduct(@RequestBody Product product){
-        this.cartService.addProductToCart(product);
+    @RequestMapping(path = "/products/{productId}", method = RequestMethod.POST)
+    public void addProduct(@PathVariable Long productId){
+        this.productId = productId;
+        this.cartService.addProductToCart(productId);
     }
 
     // usunięcie z koszyka
-    @RequestMapping(path = "/products", method = RequestMethod.DELETE)
-    public void deleteProduct(@RequestBody Product product) {
-        this.cartService.deleteProductFromCart(product);
+    @RequestMapping(path = "/products/{productID}", method = RequestMethod.DELETE)
+    public void deleteProduct(@PathVariable Long productId) {
+        this.cartService.deleteProductFromCart(productId);
     }
 
     // zwiększenie quantity dla obiektu z listy koszyka

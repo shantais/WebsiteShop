d19bcbe0fa4a3cb7d3b2a381f32570063fe435f1
WallyLDZ <waliszeksebastian@gmail.com>
2024-02-08 18:30:06 +0100

    create ContactDetailsService

--- src/main/java/com/bestbuykamps/websiteshop/business_service/ContactDetailsService.java
+++ src/main/java/com/bestbuykamps/websiteshop/business_service/ContactDetailsService.java
@@ -1,4 +1,29 @@
-package com.bestbuykamps.websiteshop.business_service;
+package com.example.myapp.services;
 
+import com.bestbuykamps.websiteshop.data_model.ContactDetails;
+import com.bestbuykamps.websiteshop.data_model.ContactDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContactDetailsService {
+
+    private final ContactDetailsRepository contactDetailsRepository;
+
+    @Autowired
+    public ContactDetailsService(ContactDetailsRepository contactDetailsRepository) {
+        this.contactDetailsRepository = contactDetailsRepository;
+    }
+
+    public ContactDetails addContactDetails(ContactDetails contactDetails) {
+        return contactDetailsRepository.save(contactDetails);
+    }
+
+    public ContactDetails updateContactDetails(ContactDetails contactDetails) {
+        return contactDetailsRepository.save(contactDetails);
+    }
+
+    public void deleteContactDetails(Long id) {
+        contactDetailsRepository.deleteById(id);
+    }
 }

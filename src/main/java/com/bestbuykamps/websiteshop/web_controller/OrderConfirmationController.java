18e7b74a9ddd913839ecb14e02df83fd26b0b131
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-02-08 19:57:39 +0100

    Creating confirmationPage

---
+++ src/main/java/com/bestbuykamps/websiteshop/web_controller/OrderConfirmationController.java
@@ -0,0 +1,20 @@
+package com.bestbuykamps.websiteshop.web_controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@Controller
+@RequestMapping("/")
+public class OrderConfirmationController {
+    @GetMapping("/confirmation")
+    public String showOrderConfirmation(Model model) {
+        Random random = new Random();
+        int orderNumber = random.nextInt(1000000);
+        model.addAttribute("orderNumber", orderNumber);
+        return "ORDER_PLACED";
+    }
+}

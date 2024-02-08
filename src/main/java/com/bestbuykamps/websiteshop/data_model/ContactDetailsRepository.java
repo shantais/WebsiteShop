963597d0e9216eacf2ee4e0fda1d953eb154fa73
Michal <19Sandriel93@gmail.com>
2024-02-08 18:30:52 +0100

    dodanie repo do contactDetails

---
+++ src/main/java/com/bestbuykamps/websiteshop/data_model/ContactDetailsRepository.java
@@ -0,0 +1,6 @@
+package com.bestbuykamps.websiteshop.data_model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactDetailsRepository extends JpaRepository<ContactDetails,Long> {
+}

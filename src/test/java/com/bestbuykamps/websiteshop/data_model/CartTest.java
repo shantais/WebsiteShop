531721bd3614a95d9aa45a0cfd1e49a6878f34a1
WallyLDZ <waliszeksebastian@gmail.com>
2024-02-12 20:26:39 +0100

    add class CartTest.java

---
+++ src/test/java/com/bestbuykamps/websiteshop/data_model/CartTest.java
@@ -0,0 +1,25 @@
+package com.bestbuykamps.websiteshop.data_model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+    private Cart cart;
+
+    @BeforeEach
+    void setUp() {
+        cart = new Cart();
+    }
+
+
+    @Test
+    void testUserId() {
+        Long userId = 123L;
+        cart.setUserId(userId);
+        assertEquals(userId, cart.getUserId());
+    }
+
+
+}

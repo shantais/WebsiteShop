016c93f59602ec24baf50fd48711cab33323a7c1
Michal <19Sandriel93@gmail.com>
2024-02-12 18:32:24 +0100

    dodanie entity i repository dla user i order

--- src/main/java/com/bestbuykamps/websiteshop/data_model/User.java
+++ src/main/java/com/bestbuykamps/websiteshop/data_model/User.java
@@ -1,21 +1,35 @@
 package com.bestbuykamps.websiteshop.data_model;
+import jakarta.persistence.*;
 
+@Entity
 public class User {
-    private final Integer userId;
-    private final String username;
+    @Id
+    private  Integer userId;
+    @Column(name = "username")
+    private  String username;
+    @Column(name = "password")
+    private  String password;
 
 
-    public User(String username, Integer userId) {
+    public User(String username, Integer userId, String password) {
         this.username = username;
         this.userId = userId;
+        this.password = password;
     }
+
+    public User() {
+
+    }
+
 
     public Integer getUserId() {
         return userId;
     }
-
     public String getUsername() {
         return username;
+    }
+    public  String getPassword() {
+        return password;
     }
 
     @Override

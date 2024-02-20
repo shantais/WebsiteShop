cdf3d2bd56413f9dd054b787df163e575dd8d2c9
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-02-20 09:43:10 +0100

    adding validation and spring security to the project

--- src/main/java/com/bestbuykamps/websiteshop/data_model/ContactDetails.java
+++ src/main/java/com/bestbuykamps/websiteshop/data_model/ContactDetails.java
@@ -1,6 +1,7 @@
 package com.bestbuykamps.websiteshop.data_model;
 
 import jakarta.persistence.*;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Table(name = "contact_details")
@@ -10,34 +11,44 @@ public class ContactDetails {
     private Long id;
 
     @Column(name = "first_name", length = 50)
+    @Length(min = 3, max = 50, message = "First name has to be between 3 and 50 characters long")
     private String firstName;
 
     @Column(name = "last_name", length = 50)
+    @Length(min = 3, max = 50, message = "Last name has to be between 3 and 50 characters long")
     private String lastName;
 
     @Column(name = "street", length = 300)
+    @Length(min = 5, max = 300, message = "Street has to be between 5 and 300 characters long")
     private String street;
 
     @Column(name = "house_number", length = 10)
+    @Length(min = 1, max = 10, message = "House number has to be between 1 and 10 characters long")
     private String houseNumber;
 
     @Column(name = "apartment_number", length = 10)
+    @Length(min = 1, max = 10, message = "Apartment number has to be between 1 and 10 characters long")
     private String apartmentNumber;
 
     @Column(name = "postal_code", length = 6)
+    @Length(min = 4, max = 6, message = "Postal code has to be between 4 and 6 characters long")
     private String postalCode;
 
     @Column(name = "city", length = 30)
+    @Length(min = 3, max = 30, message = "City has to be between 3 and 30 characters long")
     private String city;
 
     @Column(name = "phone_number", length = 9)
+    @Length(min = 6, max = 9, message = "Phone number has to be between 6 and 9 characters long")
     private String phoneNumber;
 
     @Column(name = "email", length = 30)
+    @Length(min = 6, max = 30, message = "Email has to be between 6 and 30 characters long")
     private String email;
 
 
     @Column(name = "country", length = 20)
+    @Length(min = 3, max = 20, message = "Country has to be between 3 and 20 characters long")
     private String country;
 
     @OneToOne(mappedBy = "contactDetails")

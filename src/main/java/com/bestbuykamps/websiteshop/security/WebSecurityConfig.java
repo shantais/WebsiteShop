182a938a25c291eef4c6e81529ffb38a481266aa
shantais <shantais90@gmail.com>
2024-02-21 22:24:58 +0100

    attemps to make security working: only localhost8080 works

--- src/main/java/com/bestbuykamps/websiteshop/security/WebSecurityConfig.java
+++ src/main/java/com/bestbuykamps/websiteshop/security/WebSecurityConfig.java
@@ -4,19 +4,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/",
+            "/cart/**",
+            "/cart/products/**",
+            "/api/**",
+            "/login",
+
+    };
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/css/**"),
+                                new AntPathRequestMatcher("/js/**"),
+                                new AntPathRequestMatcher("/images/**") // Allow access to static resources ^
+                        ).permitAll()
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
@@ -25,7 +42,7 @@ public class WebSecurityConfig {
                         .defaultSuccessUrl("/", true)
                         .failureUrl("/login?error=true")
                 )
-                .logout((logout) -> logout.permitAll());
+                .logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }

053acf81a462a9579ef50aa2d87f80b924477f3e
shantais <shantais90@gmail.com>
2024-02-25 18:13:43 +0100

    fixed admin authority

--- src/main/java/com/bestbuykamps/websiteshop/web_controller/AdminPanelController.java
+++ src/main/java/com/bestbuykamps/websiteshop/web_controller/AdminPanelController.java
@@ -1,19 +1,8 @@
 package com.bestbuykamps.websiteshop.web_controller;
-import com.bestbuykamps.websiteshop.business_service.CartService;
-import com.bestbuykamps.websiteshop.data_model.CartItem;
-import com.bestbuykamps.websiteshop.util.SessionUtil;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import java.util.List;
 
